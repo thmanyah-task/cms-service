8f0231f2e7aa370251c0df8d01f820a76a25f74a
kkhalid.c <kkhalid.c@tahakom.com>
2025-07-25 19:47:23 +0300

    Refactor Add New Episode To Avoid N+1 Query Issues

--- src/main/java/com/thmanyah/cms_service/episode/serviceImpl/EpisodeServiceImpl.java
+++ src/main/java/com/thmanyah/cms_service/episode/serviceImpl/EpisodeServiceImpl.java
@@ -16,6 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -28,16 +31,26 @@ public class EpisodeServiceImpl implements EpisodeService {
 
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public List<Long> addNewEpisodes(List<EpisodeDto> episodeDtoList) {
         if (episodeDtoList == null || episodeDtoList.isEmpty()) {
             throw new ValidationException("Episode list cannot be empty");
         }
-        List<Long> episodeIds = new ArrayList<>();
-        for (EpisodeDto episodeDto : episodeDtoList) {
-            Programme programme = programmeRepository.findById(episodeDto.getProgramme().getId())
-                    .orElseThrow(() -> new ValidationException("Programme not found with ID: " + episodeDto.getProgramme().getId()));
+        List<Long> programmeIds = episodeDtoList.stream()
+                .map(dto -> dto.getProgramme().getId())
+                .distinct()
+                .toList();
 
+        Map<Long, Programme> programmeMap = programmeRepository.findAllById(programmeIds).stream()
+                .collect(Collectors.toMap(Programme::getId, Function.identity()));
+
+        List<Long> episodeIds = new ArrayList<>();
+
+        for (EpisodeDto episodeDto : episodeDtoList) {
+            Programme programme = programmeMap.get(episodeDto.getProgramme().getId());
+            if (programme == null) {
+                throw new ValidationException("Programme not found with ID: " + episodeDto.getProgramme().getId());
+            }
             if (episodeDto.getDescription() == null || episodeDto.getDescription().isEmpty()) {
                 throw new ValidationException("Description Cannot Be Null");
             }
@@ -56,6 +69,7 @@ public class EpisodeServiceImpl implements EpisodeService {
             if (episodeDto.getPublishedDate() == null) {
                 throw new ValidationException("Published Date Cannot Be Null");
             }
+
             Episode episode = episodeMapper.mapToEpisodeEntity(episodeDto);
             episode.setCreatedDate(LocalDateTime.now());
             episode.setProgramme(programme);
@@ -65,6 +79,7 @@ public class EpisodeServiceImpl implements EpisodeService {
 
         return episodeIds;
     }
+
 
 
     @Override
